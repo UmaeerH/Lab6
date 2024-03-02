52f18ed116704143a7d6d90183446f582cd91b3f
Umi <umaeerh04@gmail.com>
2024-03-02 12:14:00 +0000

    Better formatting and readability

--- ex3.py
+++ ex3.py
@@ -1,6 +1,5 @@
 class Node:
     def __init__(self, value, children=None):
-    # a leaf node has an empty list of children
         if children is None:
             children = []
         self.value = value
@@ -15,14 +14,15 @@ class Node:
             depthList.append(depth)
             return None
 
-    #Should return the number of ancestors + itself, as a value. Hence, the size of the "subtree" of the inserted node
+    #Returns number of nodes including self, as size
+    #Depth/Height is indexed from 0
     def subtreeSizeRec(self):
         global depth
-        #print(f'Depth is: {depth}')    Debugging print
+        #print(f'Depth is: {depth}')                                                    #Debugging print
         if self.countChildren() != None:
             depth += 1
             for i in range(len(self.children)):
-                #print(f"Found {self.children[i].value} as a child in {self.value}")    Debugging print
+                #print(f"Found {self.children[i].value} as a child in {self.value}")    #Debugging print
                 global count
                 count += 1
                 self.children[i].subtreeSizeRec()
@@ -37,16 +37,17 @@ class Node:
         depth = 0
         self.subtreeSizeRec()
         height = max(depthList)
-        return f'Size of Subtree is {count} \nHeight of Subtree is: {height}'
+        return f'Showing info for {self.value}:\nSize of Subtree: {count} \nHeight of Subtree: {height}'
 
+#Testing
 def main():
-    leafNode1 = Node(3)
-    leafNode2 = Node(7)
-    leafNode3 = Node(9)
-    childNode1 = Node(2, [leafNode1, leafNode2])
-    childNode2 = Node(5, [leafNode3])
-    rootNode = Node(10, [childNode1, childNode2])
-    print(childNode1.subtreeSize())
+    leafNode1 = Node("leaf1")
+    leafNode2 = Node("leaf2")
+    leafNode3 = Node("leaf3")
+    childNode1 = Node("child1", [leafNode1, leafNode2])
+    childNode2 = Node("child2", [leafNode3])
+    rootNode = Node("root", [childNode1, childNode2])
+    print(leafNode2.subtreeSize())
 
 if __name__ == "__main__":
     main()
